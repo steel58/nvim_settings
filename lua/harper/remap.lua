ec60427bd0b8c944032821058d3aabfe63ab88a0
Harper Nordin <harper.nordin@gmail.com>
2024-09-16 16:15:45 -0700

    Added bracket around visual selection

--- lua/harper/remap.lua
+++ lua/harper/remap.lua
@@ -29,6 +29,13 @@ vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
+--I'm so drunk rn!!
+vim.keymap.set("v", "(", "c()<Esc>hpll")
+vim.keymap.set("v", "{", "c{}<Esc>hpll")
+vim.keymap.set("v", "[", "c[]<Esc>hpll")
+vim.keymap.set("v", "'", "c''<Esc>hpll")
+vim.keymap.set("v", '"', 'c""<Esc>hpll')
+
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
