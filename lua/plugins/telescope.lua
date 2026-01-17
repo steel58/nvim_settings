7be18fca78b6ca71e29490f2d8f6812979107a31
Harper Nordin <harper.nordin@gmail.com>
2026-01-16 20:43:00 -0800

    removed harpoon telescope window, it was hard to use for an old fuck like me

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,27 +6,28 @@ local function config()
     vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
     local conf = require("telescope.config").values
-    local harpoon = require('harpoon')
-    harpoon:setup({})
 
-    local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-        end
-    
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-        }):find()
-    end
+    -- local harpoon = require('harpoon')
+    -- harpoon:setup({})
 
-    vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+    -- local function toggle_telescope(harpoon_files)
+    --     local file_paths = {}
+    --     for _, item in ipairs(harpoon_files.items) do
+    --         table.insert(file_paths, item.value)
+    --     end
+
+    --     require("telescope.pickers").new({}, {
+    --         prompt_title = "Harpoon",
+    --         finder = require("telescope.finders").new_table({
+    --             results = file_paths,
+    --         }),
+    --         previewer = conf.file_previewer({}),
+    --         sorter = conf.generic_sorter({}),
+    --     }):find()
+    -- end
+
+    -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+    -- { desc = "Open harpoon window" })
 end
 
 return {
