c19722ae66e7c290a561a317e95084902f26f410
Harper Nordin <harper.nordin@gmail.com>
2026-02-16 07:08:15 -0800

    SHow errors keybind

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -7,3 +7,7 @@ require("lsp.go")
 
 vim.keymap.set("n", "<leader>gep", vim.diagnostic.goto_prev)
 vim.keymap.set("n", "<leader>gen", vim.diagnostic.goto_next)
+vim.keymap.set('n', 'gK', function()
+  local new_config = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = 'Toggle diagnostic virtual_lines' })
