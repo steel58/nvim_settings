a93cabb647ef1ae0087e65ba9774155b60339298
Harper Nordin <harper.nordin@wildcardcorp.com>
2026-05-11 13:51:00 -0500

    updated to nvim 0.12

--- init.lua
+++ init.lua
@@ -4,6 +4,8 @@ require("config.lazy")
 vim.api.nvim_create_autocmd("FileType", {
   callback = function()
     pcall(vim.treesitter.start)
+    vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo[0][0].foldmethod = 'expr'
   end,
 })
 
--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -4,25 +4,24 @@ local function config()
     -- REQUIRED
     harpoon:setup()
     -- REQUIRED
-    
     vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-    
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+
     vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<C-b>", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-    
+
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-p>", function() harpoon:list():prev() end)
     vim.keymap.set("n", "<C-n>", function() harpoon:list():next() end)
 end
 
-
 return {
-	"ThePrimeagen/harpoon",
-	branch = "harpoon2",
+    'ThePrimeagen/harpoon',
+    branch = 'harpoon2',
     lazy = false,
-	dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = { 'nvim-lua/plenary.nvim'},
     config = config,
 }
+
