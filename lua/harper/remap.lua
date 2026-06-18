2563f63a8f6b082fd7779b565a566cc57a4456fc
Harper Nordin <harper.nordin@gmail.com>
2026-06-18 08:55:36 -0700

    remaps and whatnot

--- lua/harper/remap.lua
+++ lua/harper/remap.lua
@@ -35,8 +35,8 @@ vim.keymap.set("v", "[", "c[]<Esc>hpll")
 vim.keymap.set("v", "'", "c''<Esc>hpll")
 vim.keymap.set("v", '"', 'c""<Esc>hpll')
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-k>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -5,9 +5,42 @@ require("lsp.clangd")
 require("lsp.java")
 require("lsp.go")
 
-vim.keymap.set("n", "<leader>gep", vim.diagnostic.goto_prev)
-vim.keymap.set("n", "<leader>gen", vim.diagnostic.goto_next)
+-- vim.keymap.set("n", "<leader>g[", function() vim.diagnostic.jump({
+--         count=1,
+--         wrap=true,
+--     })
+-- end)
+-- vim.keymap.set("n", "<leader>g]", function() vim.diagnostic.jump({
+--         count=-1,
+--         wrap=true,
+--     })
+-- end)
+-- vim.keymap.set("n", "g[", function() vim.diagnostic.jump({
+--         count=1,
+--         wrap=true,
+--         severity=vim.diagnostic.severity.ERROR,
+--     })
+-- end)
+-- vim.keymap.set("n", "g]", function() vim.diagnostic.jump({
+--         count=-1,
+--         wrap=true,
+--         severity=vim.diagnostic.severity.ERROR,
+--     })
+-- end)
+
+vim.keymap.set("n", "ge", function() vim.diagnostic.setqflist({
+    open=false,
+    severity=vim.diagnostic.severity.ERROR,
+}) end)
+
+vim.keymap.set("n", "<leader>ge", function() vim.diagnostic.setqflist({
+    open=false,
+}) end)
+vim.keymap.set("n", "gD", function()
+    require("telescope.builtin").lsp_definitions()
+end, { noremap = true, silent = true, desc = 'Go to global scope definition' })
+
 vim.keymap.set('n', 'gK', function()
-  local new_config = not vim.diagnostic.config().virtual_lines
-  vim.diagnostic.config({ virtual_lines = new_config })
+    local new_config = not vim.diagnostic.config().virtual_lines
+    vim.diagnostic.config({ virtual_lines = new_config })
 end, { desc = 'Toggle diagnostic virtual_lines' })
